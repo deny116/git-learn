b8128c60bb61bea7c42a4939f1af53e51ba19be8
Damian Kołcz <damian.kolcz@gmail.com>
2023-04-05 18:46:25 +0200

    new Class Message

--- src/main/java/pl/kolcz/damian/git/learn/GitMain.java
+++ src/main/java/pl/kolcz/damian/git/learn/GitMain.java
@@ -3,5 +3,6 @@ package pl.kolcz.damian.git.learn;
 public class GitMain {
     public static void main(String[] args) {
         System.out.println("git test");
+        System.out.println("New message");
     }
 }
